753916739bf3329dd18d38bacbc9a342e8c9ffe5
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-09 11:46:13 +0200

    Initial commit

---
+++ typescript-example/hello.ts
@@ -0,0 +1,6 @@
+const printCoord = (pt: {x: number, y:number}) => {
+    console.log(`The coordinate of x is ${pt.x}`);
+    console.log(`The coordinate of y is ${pt.y}`);
+}
+
+printCoord({x:3, y:2})
