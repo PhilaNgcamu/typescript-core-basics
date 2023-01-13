a0fcd803f33a6c825f22fc0beee5143c24ebfa6b
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-13 15:29:38 +0200

    Working with union types

--- typescript/UnionTypes/example.ts
+++ typescript/UnionTypes/example.ts
@@ -5,3 +5,8 @@ function welcomePeople(x: string[] | number) {
   } // Else "x" is a string
   else console.log(`Welcome lone traveler ${x}`); 
 }
+
+function getFirstThree(x: number[] | string) {
+    //No need to narrow down to the specific type as the number[] | string as these share the common method
+    return x.slice(0, 3)
+}
