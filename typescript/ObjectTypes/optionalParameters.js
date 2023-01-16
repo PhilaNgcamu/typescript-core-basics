91b5c9423598511c50e34793346d46f10bd689cb
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-16 13:34:51 +0200

    Optional Parameters

--- typescript/ObjectTypes/optionalParameters.js
+++ typescript/ObjectTypes/optionalParameters.js
@@ -1,5 +1,10 @@
 function printName(obj) {
-    console.log(`My name is ${obj.first} ${obj.last?.toUpperCase()}`);
+    console.log(`My name is ${obj.first}`);
 }
 printName({ first: "Philasande" });
 printName({ first: "Philasande", last: "Ngcamu" });
+//Declare clubNames with paramters with object types of strings
+function clubNames(clubs) {
+    console.log(`These are two European clubs, ${clubs.firstClub} & ${clubs.secondClub}`);
+}
+clubNames({ firstClub: "Manchester United", secondClub: "Real Madrid" });
--- typescript/ObjectTypes/optionalParameters.ts
+++ typescript/ObjectTypes/optionalParameters.ts
@@ -1,7 +1,14 @@
 function printName(obj: {first: string, last?: string}) {
-    console.log(`My name is ${obj.first} ${obj.last?.toUpperCase()}`); 
+    console.log(`My name is ${obj.first}`);
 }
 
 printName({first: "Philasande"})
 printName({first: "Philasande", last: "Ngcamu"})
+
+//Declare clubNames with paramters with object types of strings
+function clubNames(clubs: {firstClub: string, secondClub?: string}) {
+    console.log(`These are two European clubs, ${clubs.firstClub} & ${clubs.secondClub}`);
+}
+
+clubNames({firstClub: "Manchester United", secondClub: "Real Madrid"})
 
