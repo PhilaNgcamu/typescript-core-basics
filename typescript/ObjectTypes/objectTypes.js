114a8f9536295a3f1bbb615c7d826c3ac76791a6
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-16 13:15:20 +0200

    Object Types

--- typescript/ObjectTypes/objectTypes.js
+++ typescript/ObjectTypes/objectTypes.js
@@ -2,4 +2,8 @@ function printCoord(pt) {
     console.log(`The coordinate of x is ${pt.x}`);
     console.log(`The coordinate of y is ${pt.y}`);
 }
-printCoord({ x: 7, y: 9 });
+printCoord({ x: 3, y: 4 });
+function writeTwoNames(names) {
+    console.log(`The names of these best friends are ${names.first} and ${names.second}`);
+}
+writeTwoNames({ first: "Philasande", second: "Ngcamu" });
--- typescript/ObjectTypes/objectTypes.ts
+++ typescript/ObjectTypes/objectTypes.ts
@@ -1,6 +1,16 @@
-function printCoord(pt: {x:number, y:number}) {
+function printCoord(pt: {x:number , y: number}) {
     console.log(`The coordinate of x is ${pt.x}`);
     console.log(`The coordinate of y is ${pt.y}`);
 }
 
-printCoord({x:3, y:4})
+printCoord({x:3, y:4});
+
+
+//Declare the function writeTwoNames with inferred types of string in proprties of the parameter, "names".
+function writeTwoNames(names: {first: string, second: string} ) {
+    //print out two names.
+    console.log(`The names of these best friends are ${names.first} and ${names.second}`);
+   
+}
+
+writeTwoNames({first: "Philasande", second: "Ngcamu"})
