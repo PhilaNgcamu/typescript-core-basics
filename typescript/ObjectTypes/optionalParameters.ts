8573f3a04446305dd78e09cd7c47055568a80a33
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-23 14:09:36 +0200

    Object Types

--- typescript/ObjectTypes/optionalParameters.ts
+++ typescript/ObjectTypes/optionalParameters.ts
@@ -1,15 +1,9 @@
 function printName(obj: {first: string, last?: string}) {
-    console.log(`My name is ${obj.first} ${obj.last}`);
+    if(obj.last) {
+    console.log(`My name is ${obj.first} ${obj.last}`);  
+    }
+    console.log(`My name is ${obj.first}`);  
 }
 
-//the "last" property will return the undefined value if there's no string
 printName({first: "Philasande"})
 printName({first: "Philasande", last: "Ngcamu"})
-
-//Declare clubNames with paramters with object types of strings
-function clubNames(clubs: {firstClub: string, secondClub?: string}) {
-    console.log(`These are two European clubs, ${clubs.firstClub} & ${clubs.secondClub}`);
-}
-
-clubNames({firstClub: "Manchester United", secondClub: "Real Madrid"})
-
