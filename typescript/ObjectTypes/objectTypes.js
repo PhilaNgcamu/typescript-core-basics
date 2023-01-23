e4747e77acac5252c724783d4aaa757e0ca6ace7
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-23 14:00:36 +0200

    Object Types

--- typescript/ObjectTypes/objectTypes.js
+++ typescript/ObjectTypes/objectTypes.js
@@ -2,10 +2,4 @@ function printCoord(pt) {
     console.log(`The coordinate of x is ${pt.x}`);
     console.log(`The coordinate of y is ${pt.y}`);
 }
-printCoord({ x: 3, y: 4 });
-//Declare the function writeTwoNames with inferred types of string in proprties of the parameter, "names".
-function writeTwoNames(names) {
-    //print out two names.
-    console.log(`The names of these best friends are ${names.first} and ${names.second}`);
-}
-writeTwoNames({ first: "Philasande", second: "Ngcamu" });
+printCoord({ y: 88, x: 66 });
--- typescript/ObjectTypes/objectTypes.ts
+++ typescript/ObjectTypes/objectTypes.ts
@@ -1,16 +1,6 @@
-function printCoord(pt: {x:number , y: number}) {
-    console.log(`The coordinate of x is ${pt.x}`);
-    console.log(`The coordinate of y is ${pt.y}`);
+function printCoord(pt : {x: number, y: number}) {
+    console.log(`The coordinate of x is ${pt.x}`)
+    console.log(`The coordinate of y is ${pt.y}`)
 }
 
-printCoord({x:3, y:4});
-
-
-//Declare the function writeTwoNames with inferred types of string in proprties of the parameter, "names".
-function writeTwoNames(names: {first: string, second: string} ) {
-    //print out two names.
-    console.log(`The names of these best friends are ${names.first} and ${names.second}`);
-   
-}
-
-writeTwoNames({first: "Philasande", second: "Ngcamu"})
+printCoord({y:88, x:66})
