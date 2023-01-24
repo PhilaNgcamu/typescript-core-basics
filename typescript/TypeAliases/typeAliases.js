6ea36c87e6ca9849f151653b416b93d1854bd086
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-24 13:59:57 +0200

    Type Aliases

--- typescript/TypeAliases/typeAliases.js
+++ typescript/TypeAliases/typeAliases.js
@@ -3,3 +3,7 @@ function printCoord(pt) {
     console.log(`The coordinates of y is ${pt.y}`);
 }
 printCoord({ x: 12, y: 88 });
+function sanitizeInput(str) {
+    return str;
+}
+let userInput = sanitizeInput(getInput());
