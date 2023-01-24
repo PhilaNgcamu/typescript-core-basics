fe8a0a456dcb7ef34b13693da80da76234d01b19
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-24 13:22:59 +0200

    Union Types

--- typescript/UnionTypes/workingWithUnionTypes.js
+++ typescript/UnionTypes/workingWithUnionTypes.js
@@ -1,13 +1,8 @@
-function printId(id) {
-    //Since id is a union of two types, the method for the string type doesn't exist for the number type.
-    //console.log(id.toUpperCase());
+function welcomePeople(x) {
+    if (Array.isArray(x)) {
+        console.log(`Hello ${x.join(" and ")}`);
+    }
+    else
+        console.log(`Welcome, lone traveler ${x}`);
 }
-printId("2002");
-//To fix this above error, "narrow" the variable to a specific type by checking if the argument is a number or a string type
-function printID(id) {
-    if (typeof id === "number")
-        return Number.isInteger(id);
-    return id.toUpperCase();
-}
-printID(2003);
-printID("Philasande");
+welcomePeople(["Phila", "Siya"]);
--- typescript/UnionTypes/workingWithUnionTypes.ts
+++ typescript/UnionTypes/workingWithUnionTypes.ts
@@ -1,17 +1,8 @@
-function printId(id: number | string) {
-    //Since id is a union of two types, the method for the string type doesn't exist for the number type.
-    //console.log(id.toUpperCase());
+function welcomePeople(x: string | string[]) {
+    if(Array.isArray(x)) {
+        console.log(`Hello ${x.join(" and ")}`); 
+    } else console.log(`Welcome, lone traveler ${x}`);
     
 }
 
-printId("2002")
-
-//To fix this above error, "narrow" the variable to a specific type by checking if the argument is a number or a string type
-function printID(id: number | string) {
-    if(typeof id === "number") return Number.isInteger(id);
-    return id.toUpperCase()
-}
-
-printID(2003)
-printID("Philasande")
-
+welcomePeople(["Phila", "Siya"])
