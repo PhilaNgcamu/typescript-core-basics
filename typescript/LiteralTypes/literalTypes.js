2f76be266dcc8a8171c942e4bcf9778c741bd18d
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-25 12:03:26 +0200

    Literal Types

--- typescript/LiteralTypes/literalTypes.js
+++ typescript/LiteralTypes/literalTypes.js
@@ -4,3 +4,9 @@ const constantString = "Hello, World"; // where the type of "constantString" is 
 let x = "hello"; //where "hello" is the literal string to be assigned to x (the only possible)
 //The following is an error becuse x is the literal type of "hello" only:
 //x = "World";
+function printText(s, alignment) {
+    console.log(`Align this text, "${s}" to ${alignment}`);
+} //where, "s" accepts any possible string type & "alignment" only accepts "left", "right" and "center" types
+printText("Nice one, Phila", "left");
+//But there will be an error is alignment has an invalid type
+//printText("Nice one, Phila", "bottom")
