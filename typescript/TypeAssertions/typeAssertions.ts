1d8193e799affb0efbad2de3c551ae5f0d907f79
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-25 11:35:46 +0200

    Type Assertions

--- typescript/TypeAssertions/typeAssertions.ts
+++ typescript/TypeAssertions/typeAssertions.ts
@@ -1,1 +1,5 @@
-const myCanvas = document.getElementById("main-canvas") as HTMLCanvasElement;
+//Sometimes, Typescript doesn't know what the dom(an element) will return "some" type. You have to tell it that you know it will return the relevant type.
+
+const area = document.getElementById("area") as HTMLAreaElement;
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement
