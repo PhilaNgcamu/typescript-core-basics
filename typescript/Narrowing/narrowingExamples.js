9a1b9f0cc1ccc0c3e6617998da48ce6f8ea914c0
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-02 13:05:44 +0200

    Narrowing

--- typescript/Narrowing/narrowingExamples.js
+++ typescript/Narrowing/narrowingExamples.js
@@ -1,35 +1,5 @@
 function padLeft(padding, input) {
-    if (typeof padding === "number")
-        return " ".repeat(padding) + input;
-    return padding + input;
-}
-padLeft(5, "Philasande");
-function printAll(strs) {
-    if (strs && typeof strs === "object") {
-        for (const s of strs) {
-            console.log(s);
-        }
-    }
-    else if (typeof strs === "string") {
-        console.log(strs);
-    }
-    else {
-        // do nothing
-    }
-}
-function example(x, y) {
-    if (x === y) {
-        console.log(x.toUpperCase());
-        console.log(y.toLowerCase());
-    }
-    else {
-        console.log(x);
-        console.log(y);
-    }
-}
-example("philasande", "NGCAMU");
-function move(animal) {
-    if ("swim" in animal)
-        return animal.swim();
-    return animal.fly();
+    //This below will throw an error type becuse "padding" can be a string type whicj isn't assignable to the repeat method:
+    // return "  ".repeat(padding)
+    throw new Error("return type string");
 }
--- typescript/Narrowing/narrowingExamples.ts
+++ typescript/Narrowing/narrowingExamples.ts
@@ -1,57 +1,6 @@
 function padLeft(padding: number | string, input: string): string {
-    if(typeof padding === "number") return " ".repeat(padding) + input;
-    return padding + input
+    //This below will throw an error type becuse "padding" can be a string type whicj isn't assignable to the repeat method:
+   // return "  ".repeat(padding)
+   throw new Error("return type string")
 }
 
-padLeft(5, "Philasande");
-
-function printAll(strs: string | string[] | null) {
-    if(strs && typeof strs === "object") {
-        for(const s of strs) {
-            console.log(s);
-        }
-    } else if (typeof strs === "string") {
-        console.log(strs);
-      } else {
-        // do nothing
-    }
-}
-
-function example(x: string | number, y: string | boolean) {
-    if(x === y) {
-        console.log(x.toUpperCase());
-        console.log(y.toLowerCase());
-    } else {
-        console.log(x);
-        console.log(y);
-    }
-}
-
-example("philasande", "NGCAMU");
-
-type Fish = {swim: () => void};
-type Bird = {fly: () => void};
-type Human = { swim?: () => void; fly?: () => void };
-
-
-function move(animal: Fish | Bird | Human) {
-    if("swim" in animal) return animal;
-    return animal
-}
-
-function logValue(x: Date | string) {
-    if(x instanceof Date) return x.toDateString();
-    return x.toLowerCase()
-
-}
-
-function isFish(pet: Fish | Bird): pet is Fish {
-    return !(pet as Fish).swim
-}
-
-declare function getSmallPet(): Fish | Bird;
-
-let pet = getSmallPet();
- 
-if(isFish(pet)) pet.swim();
-else pet.fly()
